a596680457b6ccfbe202175021eb067688106c28
sasa <sasa.franic@gmail.com>
2025-04-17 20:58:19 +0200

    dodanto

---
+++ funkcije/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funkcije</title>
+</head>
+<body>
+    <h1>Funkcije</h1>
+    <h2>Prvi zadatak</h2>
+    <hr>
+    <?php
+        function vratiTekst() {
+            return "Tekst tekst Tekst";
+        }
+
+        $tekst = vratiTekst();
+        echo $tekst;
+
+        
+    ?>
+    
+</body>
+</html>
