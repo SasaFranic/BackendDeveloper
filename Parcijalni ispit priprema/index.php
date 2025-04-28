4144627f396c899e6d89011a66231b3e547c6504
sasa <sasa.franic@gmail.com>
2025-04-28 20:54:02 +0200

    done

---
+++ Parcijalni ispit priprema/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vjezba - test</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Vjezba - test</h1>
+    <hr>
+
+    <main class= "main">
+        <form action = "">
+            <label>
+                Upišite riječ: <br>
+                <input type="text" name="rijec">
+            </label>
+            <br><br>
+            <input type="submit" value="pošalji">
+        </form>
+
+        <table border="1">
+            <tr>
+            <th>Riječi</th>
+            <th>Broj slova</th>
+            <th>Broj samoglasnika</th>
+            <th>Broj suglasnika</th>
+            </tr>
+
+            <tr>
+               <td>Papa</td>
+               <td>4</td>
+                <td>2</td>
+                <td>2</td>
+            </tr>
+
+            <tr>
+                <td>Crkva</td>
+                <td>5</td>
+                <td>1</td>
+                <td>4</td>
+            </tr>
+
+        </table>
+    </main>
+        
+</body>
+</html>
