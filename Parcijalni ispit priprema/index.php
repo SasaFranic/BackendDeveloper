8a812229ced96e465eea66d8103b8c796013e767
sasa <sasa.franic@gmail.com>
2025-04-29 20:12:54 +0200

    29042025

--- Parcijalni ispit priprema/index.php
+++ Parcijalni ispit priprema/index.php
@@ -1,3 +1,94 @@
+<?php
+    define('FILE_PATH', 'words.json');
+
+    function izracunajBrojSlova($nekaRijec)
+    {
+        return strlen($nekaRijec);
+    }
+
+    function izracunajBrojSamoglasnika($nekaRijec)
+    {
+        $rijec = strtolower($nekaRijec);
+        $samoglasnici = ['a', 'e', 'i', 'o', 'u'];
+        $slovaURijeci = preg_split('//u', $rijec);
+        
+
+        $brojSamoglasnika = 0;
+        foreach ($slovaURijeci as $slovo) {
+            if (in_array($slovo, $samoglasnici)) {
+                $brojSamoglasnika++;
+            }
+        }
+        return $brojSamoglasnika;
+    }
+
+
+    function izracunajBrojSuglasnika($nekaRijec)
+    {
+        return izracunajBrojSlova($nekaRijec) - izracunajBrojSamoglasnika($nekaRijec);
+    }
+
+    function citajJsonDatoteku($filePath)
+    {
+        $wordsJson = file_get_contents($filePath, false);
+        $nizRijeci = json_decode($wordsJson);
+        return ($nizRijeci);
+
+    }
+
+        function dodajRijec($word, $nizRijeci) 
+            {
+            $trimmedRijec = trim($word);
+            if (strlen($trimmedRijec) == 0) return; 
+            
+            $brojSlova = izracunajBrojSlova($trimmedRijec);
+            
+            $brojSamoglasnika = izracunajBrojSamoglasnika($trimmedRijec);
+
+            $brojSuglasnika = izracunajBrojSuglasnika($trimmedRijec);
+
+            $nizrijeci[] = $trimmedRijec;
+
+            $rijeciJson = json_encode($nizrijeci,);
+            file_put_contents(FILE_PATH, $rijeciJson);
+            header('Location: ' . $_SERVER['PHP_SELF']);
+            }
+
+
+   
+    $rijeci = citajJsonDatoteku(FILE_PATH);
+
+
+
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+        {
+            echo 'Ovo je POST metoda.';
+            echo '<br><br>';
+
+
+            $rijec = $_POST['rijec'];
+
+            
+            dodajRijec($rijec, $rijeci);
+
+        }
+
+ 
+
+      
+
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') 
+        {
+        echo 'Ovo je GET metoda.';
+        echo '<br><br>';
+        }
+       
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,10 +102,10 @@
     <hr>
 
     <main class= "main">
-        <form action = "">
+        <form action = "" method="POST">
             <label>
                 Upišite riječ: <br>
-                <input type="text" name="rijec">
+                <input type="text" name="rijec" required>
             </label>
             <br><br>
             <input type="submit" value="pošalji">
@@ -28,19 +119,21 @@
             <th>Broj suglasnika</th>
             </tr>
 
-            <tr>
-               <td>Papa</td>
-               <td>4</td>
-                <td>2</td>
-                <td>2</td>
-            </tr>
+        <?php
+            foreach ($rijeci as $rijecUNizu) {
+                echo '<tr>';
+                echo '<td>' . $rijecUNizu . '</td>';
+                echo '<td>' . izracunajBrojSlova($rijecUNizu) . '</td>';
+                echo '<td>' . izracunajBrojSamoglasnika($rijecUNizu) . '</td>';
+                echo '<td>' . izracunajBrojSuglasnika($rijecUNizu) . '</td>';
+                echo '</tr>';
 
-            <tr>
-                <td>Crkva</td>
-                <td>5</td>
-                <td>1</td>
-                <td>4</td>
-            </tr>
+            }
+
+
+        ?>
+
+
 
         </table>
     </main>
